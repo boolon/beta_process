6c8fdf8f93031d8ab368d813195e35483f2d6e59
Hector <sacdesable123@gmail.com>
2020-03-31 21:24:11 +0200

    update

--- draw.py
+++ draw.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-def plot_bernouli_process(X):
+def plot_bernouli_process(X, c, gamma):
     """
     X should correspond to the X1...n
     """
@@ -9,4 +9,5 @@ def plot_bernouli_process(X):
     for i in range(n):
         for el in X[i]:
             plt.plot([el, el],[i, i+1], "k")
+            plt.title("Beta process for {} samples (c = {},γ = {})".format(n, c, gamma))
     plt.show()
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-import sys
+import sys, getopt
 from draw import plot_bernouli_process
 from beta_process import *
 
 
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"c:g:n:",["gamma="])
+    except getopt.GetoptError:
+        sys.exit(2)
+
+    c = 1
+    gamma = 10
+    n = 20
+    for opt, arg in opts:
+        if opt in ("-c"):
+            c = float(arg)
+        elif opt in ("-n"):
+            n = int(arg)
+        elif opt in ("-g","--gamma"):
+            gamma = float(arg)
+
+    X = associated_bernoulli_process(b0_continuous, c, gamma, n = n)
+    plot_bernouli_process(X, c, gamma)
+
 if __name__ == "__main__":
-    X = associated_bernoulli_process(b0_continuous, 1, 10)
-    plot_bernouli_process(X)
+    main(sys.argv[1:])
     sys.exit(0)
